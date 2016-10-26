5586ab93004efbe77d3332a723dbcc990fe66eb4
FuYaoDe <dan82625@gmail.com>
2016-10-26 13:29:42 +0800

    add admin

--- config/init/jason/index.js
+++ config/init/jason/index.js
@@ -9,9 +9,44 @@ const performances = require('./data/Performance.json');
 
 module.exports.init = async () => {
   try {
-    // const {environment} = sails.config;
-    //
-    // sails.log.debug('>>>> config/init/jason >>>>');
+    const {environment} = sails.config;
+
+    sails.log.debug('>>>> config/init/jason >>>>');
+    let newMenuItems = [
+      { icon: 'home', href: '/admin/dashboard', title: '控制台', sequence: 0},
+      { icon: 'wrench', href: '#', title: '資料維護', sequence: 1},
+      // { icon: 'puzzle-piece', href: '#', title: '實驗室', sequence: 2},
+
+      // { href: '/admin/user', title: '會員資料', sequence: 20, ParentMenuItemId: 2},
+      // { href: '/admin/post', title: '內容資料', sequence: 30, ParentMenuItemId: 2},
+      { href: '/admin/message', title: '產品介紹管理', sequence: 20, ParentMenuItemId: 2},
+      { href: '/admin/message', title: '周邊配件管理', sequence: 30, ParentMenuItemId: 2},
+      { href: '/admin/message', title: '工程實績管理', sequence: 40, ParentMenuItemId: 2},
+
+      // { href: '/admin/labfnp/recipe', title: '配方資料', sequence: 40, ParentMenuItemId: 2},
+      // { href: '/admin/labfnp/scent', title: '香味分子', sequence: 50, ParentMenuItemId: 2},
+      // { href: '/admin/labfnp/scentnote', title: '香調', sequence: 60, ParentMenuItemId: 2},
+      // { href: '/admin/labfnp/feeling', title: '感覺', sequence: 70, ParentMenuItemId: 2},
+      // { href: '/admin/slogan', title: '口號', sequence: 80, ParentMenuItemId: 2},
+      // { href: '/admin/allpay', title: '訂單', sequence: 90, ParentMenuItemId: 2},
+      // { href: '/admin/mock', title: '隨機資料表', sequence: 20, ParentMenuItemId: 3},
+      // { href: '/admin/facebook/feed', title: '動態', sequence: 110, ParentMenuItemId: 2}
+
+    ]
+
+    let title = newMenuItems.map(item => item.title)
+    let findMenuItems = await MenuItem.findAll({where:{title}})
+    let findTitle = findMenuItems.map(item => item.title)
+
+    let createMenuItems = title.reduce((result, title, index) => {
+      if(findTitle.indexOf(title) == -1){
+        result.push(newMenuItems[index])
+        return result
+      }
+      return result
+    }, [])
+
+    await MenuItem.bulkCreate(createMenuItems);
     //
     // const productGroups = [
     //   {
