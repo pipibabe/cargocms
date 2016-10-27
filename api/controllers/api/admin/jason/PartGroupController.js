47009779abfdb255e1b9472d269eb7e01481a71e
KenYA <tikka0910@gmail.com>
2016-10-27 17:34:28 +0800

    新增 Part 專用的 Group Controller

--- api/controllers/api/admin/jason/PartGroupController.js
+++ api/controllers/api/admin/jason/PartGroupController.js
@@ -10,6 +10,7 @@ module.exports = {
       if (serverSidePaging) {
         result = await PagingService.process({query, modelName});
       } else {
+        query.where = { type: 'part'};
         const items = await sails.models[modelName].findAll(query);
         result = { data: { items } };
       }
@@ -18,4 +19,55 @@ module.exports = {
       res.serverError(e);
     }
   },
+  findOne: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await Group.findById(id);
+      res.ok({ data: { item } });
+    } catch (e) {
+      res.serverError(e);
+    }
+  },
+
+  create: async (req, res) => {
+    try {
+      let data = req.body;
+      
+      data.name = null;
+      data.sourceId = null;
+      const item = await Group.create(data);
+      const message = 'Create success.';
+      res.ok({ message, data: { item } });
+    } catch (e) {
+      res.serverError(e);
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      let data = req.body;
+      const message = 'Update success.';
+
+      data.name = null;
+      data.sourceId = null;
+      const item = await Group.update(data ,{
+        where: { id, },
+      });
+      res.ok({ message, data: { item } });
+    } catch (e) {
+      res.serverError(e);
+    }
+  },
+
+  destroy: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await Group.deleteById(id);
+      const message = 'Delete success.';
+      res.ok({ message, data: { item } });
+    } catch (e) {
+      res.serverError(e);
+    }
+  }
 }
