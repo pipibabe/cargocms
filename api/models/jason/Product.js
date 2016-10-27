22860a70f470452242794df07274003c8e2806f0
KenYA <tikka0910@gmail.com>
2016-10-27 11:38:21 +0800

    Product model 增加 Post 、Group 虛擬欄位

--- api/models/jason/Product.js
+++ api/models/jason/Product.js
@@ -12,6 +12,141 @@ module.exports = {
     introduction: {
       type: Sequelize.STRING,
     },
+    postTitle: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let postTitle = '';
+          if(post){
+            postTitle = post.title;
+          }
+          return postTitle;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+    postContent: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let postContent = '';
+          if(post){
+            postContent = post.content;
+          }
+          return postContent;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+    postUrl: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let postUrl = '';
+          if(post){
+            postUrl = post.url;
+          }
+          return postUrl;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+    postAbstract: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let postAbstract = '';
+          if(post){
+            postAbstract = post.abstract;
+          }
+          return postAbstract;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+    postCoverType: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let postCoverType = '';
+          if(post){
+            postCoverType = post.coverType;
+          }
+          return postCoverType;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+    postCoverUrl: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let postCoverUrl = '';
+          if(post){
+            postCoverUrl = post.coverUrl;
+          }
+          return postCoverUrl;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+    postType: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let postType = '';
+          if(post){
+            postType = post.type;
+          }
+          return postType;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+    groupId: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let groupId = '';
+          if(post && post.Group){
+            groupId = post.Group.id;
+          }
+          return groupId;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    },
+    groupTitle: {
+      type: Sequelize.VIRTUAL,
+      get: function() {
+        try {
+          const post = this.getDataValue('Post');
+          let groupTitle = '';
+          if(post && post.Group){
+            groupTitle = post.Group.title;
+          }
+          return groupTitle;
+        } catch (e) {
+          sails.log.error(e);
+        }
+      }
+    }
   },
   associations: () => {
     Product.belongsTo(Post);
@@ -19,7 +154,16 @@ module.exports = {
     Product.hasMany(File);
   },
   options: {
-    classMethods: {},
+    classMethods: {
+      deleteById: async (id) => {
+        try {
+          return await Product.destroy({ where: { id } });
+        } catch (e) {
+          sails.log.error(e);
+          throw e;
+        }
+      },
+    },
     instanceMethods: {},
     hooks: {}
   }
