d47a09d2309b24617bcd7232becb3ac888eca6c5
KenYA <tikka0910@gmail.com>
2016-10-28 21:46:50 +0800

    尚未使用 ServerSidePaging。 res.ok 回傳值都增加 Group 提供各個按鈕「連結」使用 讓連結後方都能帶對應的 type 分類

--- api/controllers/admin/jason/GroupController.js
+++ api/controllers/admin/jason/GroupController.js
@@ -2,27 +2,31 @@ module.exports = {
   index: async (req, res) => {
     res.ok({
       view: true,
-      serverSidePaging: true,
-      layout: 'admin/default/index'
+      serverSidePaging: false,
+      layout: 'admin/default/index',
+      group: req.query.type
     });
   },
   create: async (req, res) => {
     res.ok({
       view: true,
-      layout: 'admin/default/create'
+      layout: 'admin/default/create',
+      group: req.query.type
     });
   },
   edit: async (req, res) => {
     res.ok({
       view: true,
-      layout: 'admin/default/edit'
+      layout: 'admin/default/edit',
+      group: req.query.type
     });
 
   },
   show: async (req, res) => {
     res.ok({
       view: true,
-      layout: 'admin/default/show'
+      layout: 'admin/default/show',
+      group: req.query.type
     });
   },
 }
