cd9e9bd3e1524c8f42998cd7a81d3e7c644cf945
dd-han <han@dd-han.tw>
2016-11-29 18:04:07 +0800

    wait server 10 second, add tag for test, costum test

---
+++ test-stress.md
@@ -0,0 +1,24 @@
+# 進行壓力測試
+
+## 進行方式
+
+`npm run test-stress`
+
+## 測試報告
+
+你可以在 `test/stress` 裡面找到：
+
+- `output_serverMemory.xls` ：伺服器記憶體用量的Excel檔（實際上是TXT，為了方便開啟副檔名為xls）
+- `output_client` ：裡面會有 (https://artillery.io/)[] 輸出資料，包含成果與 `stdout` 、 `stderr`
+
+## 可用環境變數
+
+以下環境變數對誰都有效
+
+- `export STRESS_TAG="v7"` ：將 `log 檔存到 test/stress/v7` 裡面，對照不同狀態會方便很多。
+
+以下環境變數僅對壓力測試有效，流程測試需要直接修改 `.yaml` 才會有效。
+
+- `export STRESS_DURATION="30"` ：將壓力測試時間改為 30 秒
+- `export STRESS_RATE="30"` ：將壓力測試人數改為 30 人
+- `export STRESS_NUM="10"` ：將每個人的請求數量改為 10 個
--- test/stress/Traffic.spec.js
+++ test/stress/Traffic.spec.js
@@ -1,58 +1,84 @@
 describe("traffic test", function() {
   this.timeout(10000000);
   const fs = require('fs');
-  const logPath = "test/stress/"
-  const clientLogPath = logPath+"output_client/";
   const spawn = require('child_process').spawn;
+  let logPath = "test/stress/";
+  if (process.env.STRESS_TAG) {
+    logPath += process.env.STRESS_TAG;
+  }
+  const clientLogPath = logPath+"output_client/";
+
   let baseURL;
   before(function(done) {
     baseURL = "http://localhost:"+sails.config.port+"/";
-    done();
+    setTimeout(function(){ 
+      done();
+    }, 10000);
   });
+
+  let testParameter = {
+    duration: "60",
+    rate: "10",
+    num: "20"
+  };
+  if (process.env.STRESS_DURATION) {
+    testParameter.duration = process.env.STRESS_DURATION;
+  }
+  if (process.env.STRESS_RATE) {
+    testParameter.rate = process.env.STRESS_RATE;
+  }
+  if (process.env.STRESS_NUM) {
+    testParameter.num = process.env.STRESS_NUM;
+  }
+
+  let simpleTraffic = function(fileName,url,beforeTestMemoryUsage,done) {
+    console.log("testing URL: "+url);
+    const command = spawn('artillery', ['quick',
+      '--duration',testParameter.duration,
+      '--rate',testParameter.rate,
+      '--num',testParameter.num,
+      url,
+      '--output',jsonFileName]);
+
+    let jsonFileName = clientLogPath + fileName + ".json";
+    command.stdout.on('data', (data) => {
+      let stdoutFile = clientLogPath + fileName + "-stdout.txt";
+      fs.appendFile(stdoutFile, data , 'utf8')
+    });
+
+    command.stderr.on('data', (data) => {
+      let stdoutFile = clientLogPath + fileName + "-stderr.txt";
+      fs.appendFile(stdoutFile, data , 'utf8')
+    });
+
+    command.on('close', (code) => {
+      let afterTestMemoryUsage = process.memoryUsage();
+      console.log("=== before traffic test memoryUsage ===");
+      console.log(beforeTestMemoryUsage);
+      console.log("=== after traffic test memoryUsage ===");
+      console.log(afterTestMemoryUsage);
+
+      if (code>=0) {
+        console.log("full log available at "+logPath);
+        done();
+      } else {
+        throw("artillery exit with error! log here: "+clientLogPath);
+      }
+    });
+  }
 
   it("make an simple traffic test on index", async function(done) {
     try {
       const fileName = "index";
       let url = baseURL;
-      let jsonFileName = clientLogPath + fileName + ".json";
 
       let beforeTestMemoryUsage = process.memoryUsage();
-      console.log("testing URL: "+url);
-      const command = spawn('artillery', ['quick',
-        '--duration','60',
-        '--rate','10',
-        '-n','20',
-        url,
-        '--output',jsonFileName]);
-
-      command.stdout.on('data', (data) => {
-        let stdoutFile = clientLogPath + fileName + "-stdout.txt";
-        fs.appendFile(stdoutFile, data , 'utf8')
-      });
-
-      command.stderr.on('data', (data) => {
-        let stdoutFile = clientLogPath + fileName + "-stderr.txt";
-        fs.appendFile(stdoutFile, data , 'utf8')
-      });
-
-      command.on('close', (code) => {
-        let afterTestMemoryUsage = process.memoryUsage();
-        console.log("=== before traffic test memoryUsage ===");
-        console.log(beforeTestMemoryUsage);
-        console.log("=== after traffic test memoryUsage ===");
-        console.log(afterTestMemoryUsage);
-
-        console.log(`child process exited with code ${code}`);
-        if (code>=0) {
-          console.log("full log available at "+logPath);
-          done();
-        } else {
-          throw("process exit with error!");
-        }
-      });
+      simpleTraffic(fileName,url,beforeTestMemoryUsage,done);
 
     } catch (e) {
       done(e);
     }
   });
+
+
 });
