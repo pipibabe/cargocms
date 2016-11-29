d76defcf6c1c511373132b282f05fafa2f2e20ef
dd-han <han@dd-han.tw>
2016-11-29 18:02:59 +0800

    add title field

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -9,14 +9,20 @@ before(function(done) {
   if (process.env.TEST_MODE == "STRESS")  {
     // ref from https://segmentfault.com/a/1190000004888348
     var fs = require('fs');
-    // (see note below)
+    var logFileName = './test/stress/output_serverMemory.xls';
+    // write header
+    fs.appendFile(logFileName, 
+      "memory-rss.rss" + '\t' +
+      "memory-heapUsed" + '\t' + 
+      "memory-heapTotal" + '\n', 'utf8');
+    // write data
     setInterval(function takeSnapshot() {
       var mem = process.memoryUsage();
-      fs.appendFile('./test/stress/output_serverMemory.xls', 
+      fs.appendFile(logFileName, 
         mem.rss / 1024 / 1024 + '\t' +
         mem.heapUsed / 1024 / 1024 + '\t' + 
         mem.heapTotal / 1024 / 1024 + '\n', 'utf8');
-    }, 1000); // Snapshot every second
+    }, 1000);
     config.environment = 'production';
   }
   else {
