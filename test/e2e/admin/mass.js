74828f29793130d685d7b8bd9cabd27503f118f5
dd-han <han@dd-han.tw>
2016-12-01 18:21:27 +0800

    loading 100 page on admin

--- test/e2e/admin/mass.js
+++ test/e2e/admin/mass.js
@@ -1,7 +1,9 @@
 require("../../bootstrap.test.js")
 import {login, logout, startLogging} from "../../util/e2eHelper.js"
 
-describe('test browser admin pages', () => {
+describe('test browser admin pages', function() {
+  this.timeout(10000000);
+
   before(async (done)=>{
     try {
       console.log("=== admin login ===");
@@ -22,12 +24,64 @@ describe('test browser admin pages', () => {
     }
   })
 
-  it.only('quick switching admin paging',async (done) => {
+  it('loading 104 page of user',async (done) => {
     try {
       await browser.url('/admin/#/admin/user');
       await browser.pause(1000);
-      await browser.url('/admin/#/admin/post');
-      await browser.pause(10000);
+      await browser.click('#main-table_paginate > ul > li:nth-child(3) > a');
+      await browser.pause(100);
+      await browser.click('#main-table_paginate > ul > li:nth-child(4) > a');
+      await browser.pause(100);
+      await browser.click('#main-table_paginate > ul > li:nth-child(5) > a');
+      for(var i=0;i<100;i++) {
+        await browser.pause(100);
+        await browser.click('#main-table_paginate > ul > li:nth-child(6) > a');
+      }
+      await browser.screenshot();
+
+      done();
+    } catch (e) {
+      console.error(e.stack);
+      done(e);
+    }
+  });
+
+  it('loading 104 page of recipe',async (done) => {
+    try {
+      await browser.url('/admin/#/admin/labfnp/recipe');
+      await browser.pause(1000);
+      await browser.click('#main-table_paginate > ul > li:nth-child(3) > a');
+      await browser.pause(100);
+      await browser.click('#main-table_paginate > ul > li:nth-child(4) > a');
+      await browser.pause(100);
+      await browser.click('#main-table_paginate > ul > li:nth-child(5) > a');
+      for(var i=0;i<100;i++) {
+        await browser.pause(100);
+        await browser.click('#main-table_paginate > ul > li:nth-child(6) > a');
+      }
+      await browser.screenshot();
+
+      done();
+    } catch (e) {
+      console.error(e.stack);
+      done(e);
+    }
+  });
+
+  it('loading 104 page of feeling',async (done) => {
+    try {
+      await browser.url('/admin/#/admin/labfnp/feeling');
+      await browser.pause(1000);
+      await browser.click('#main-table_paginate > ul > li:nth-child(3) > a');
+      await browser.pause(100);
+      await browser.click('#main-table_paginate > ul > li:nth-child(4) > a');
+      await browser.pause(100);
+      await browser.click('#main-table_paginate > ul > li:nth-child(5) > a');
+      for(var i=0;i<100;i++) {
+        await browser.pause(100);
+        await browser.click('#main-table_paginate > ul > li:nth-child(6) > a');
+      }
+      await browser.screenshot();
 
       done();
     } catch (e) {
