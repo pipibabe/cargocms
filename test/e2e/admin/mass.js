b0a66b2e2ecbd2d4eb93e128fa6dcdd522e17f4f
dd-han <han@dd-han.tw>
2016-12-02 13:37:08 +0800

    auto add fakeuser for e2e-stress test

--- test/e2e/admin/mass.js
+++ test/e2e/admin/mass.js
@@ -1,4 +1,5 @@
 require("../../bootstrap.test.js")
+let user = require('../../../config/init/fakeusers');
 import {login, logout, startLogging} from "../../util/e2eHelper.js"
 
 describe('test browser admin pages', function() {
@@ -6,6 +7,9 @@ describe('test browser admin pages', function() {
 
   before(async (done)=>{
     try {
+      // add fake user data
+      await user.init();
+
       console.log("=== admin login ===");
       await login("admin");
       // 開啟分頁之後，PID會變化...
@@ -24,63 +28,27 @@ describe('test browser admin pages', function() {
     }
   })
 
-  it('loading 104 page of user',async (done) => {
+  it('loading 80 page of user',async (done) => {
     try {
       await browser.url('/admin/#/admin/user');
+      // test different page
+      // await browser.url('/admin/#/admin/labfnp/feeling');
+      // await browser.url('/admin/#/admin/labfnp/recipe');
       await browser.pause(1000);
       await browser.click('#main-table_paginate > ul > li:nth-child(3) > a');
       await browser.pause(100);
       await browser.click('#main-table_paginate > ul > li:nth-child(4) > a');
       await browser.pause(100);
       await browser.click('#main-table_paginate > ul > li:nth-child(5) > a');
-      for(var i=0;i<100;i++) {
+      for(var i=0;i<74;i++) {
         await browser.pause(100);
         await browser.click('#main-table_paginate > ul > li:nth-child(6) > a');
       }
-      await browser.screenshot();
-
-      done();
-    } catch (e) {
-      console.error(e.stack);
-      done(e);
-    }
-  });
-
-  it('loading 104 page of recipe',async (done) => {
-    try {
-      await browser.url('/admin/#/admin/labfnp/recipe');
+      await browser.pause(100);
+      await browser.click('#main-table_paginate > ul > li:nth-child(7) > a');
+      await browser.pause(100);
+      await browser.click('#main-table_paginate > ul > li:nth-child(8) > a');
       await browser.pause(1000);
-      await browser.click('#main-table_paginate > ul > li:nth-child(3) > a');
-      await browser.pause(100);
-      await browser.click('#main-table_paginate > ul > li:nth-child(4) > a');
-      await browser.pause(100);
-      await browser.click('#main-table_paginate > ul > li:nth-child(5) > a');
-      for(var i=0;i<100;i++) {
-        await browser.pause(100);
-        await browser.click('#main-table_paginate > ul > li:nth-child(6) > a');
-      }
-      await browser.screenshot();
-
-      done();
-    } catch (e) {
-      console.error(e.stack);
-      done(e);
-    }
-  });
-
-  it('loading 104 page of feeling',async (done) => {
-    try {
-      await browser.url('/admin/#/admin/labfnp/feeling');
-      await browser.pause(1000);
-      await browser.click('#main-table_paginate > ul > li:nth-child(3) > a');
-      await browser.pause(100);
-      await browser.click('#main-table_paginate > ul > li:nth-child(4) > a');
-      await browser.pause(100);
-      await browser.click('#main-table_paginate > ul > li:nth-child(5) > a');
-      for(var i=0;i<100;i++) {
-        await browser.pause(100);
-        await browser.click('#main-table_paginate > ul > li:nth-child(6) > a');
-      }
       await browser.screenshot();
 
       done();
