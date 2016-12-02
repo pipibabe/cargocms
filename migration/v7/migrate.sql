203be10166bb3b7c0cfc1d0dd09017be9b1bfd58
dd-han <han@dd-han.tw>
2016-12-02 15:48:50 +0800

    fix migrate

--- migration/v7/migrate.sql
+++ migration/v7/migrate.sql
@@ -3,5 +3,5 @@ ALTER TABLE `Images` ADD `fileName` VARCHAR(128) NOT NULL AFTER `filePath`;
 ALTER TABLE `Images` ADD `accessPath` VARCHAR(128) NOT NULL AFTER `hash`;
 ALTER TABLE `Images` ADD `title` VARCHAR(64) NOT NULL AFTER `storage`, ADD `description` VARCHAR(255) NOT NULL AFTER `title`, ADD `memo` VARCHAR(255) NOT NULL AFTER `description`;
 
-UPDATE `Images` SET `accessPath` = REPLACE(filePath,'file:///home/ubuntu/cargocms/.tmp/public/uploads/','') WHERE `filePath` LIKE 'file:///home/ubuntu/cargocms/.tmp/public/uploads/%'
+UPDATE `Images` SET `accessPath` = REPLACE(filePath,'file:///home/ubuntu/cargocms/.tmp/public/','') WHERE `filePath` LIKE 'file:///home/ubuntu/cargocms/.tmp/public/uploads/%'
 UPDATE `Images` SET `fileName` = REPLACE(filePath,'file:///home/ubuntu/cargocms/.tmp/public/uploads/','') WHERE `filePath` LIKE 'file:///home/ubuntu/cargocms/.tmp/public/uploads/%'
