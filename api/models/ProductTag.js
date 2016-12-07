8913120f59a4956501e6fb4c8e52fd840dad68fa
KenYA <tikka0910@gmail.com>
2016-12-07 15:52:13 +0800

    ProductTag 資料表初始（尚未關聯）

---
+++ api/models/ProductTag.js
@@ -0,0 +1,38 @@
+module.exports = {
+  attributes: {
+
+    // product_id: {
+    //   Sequelize.INTEGER(11),
+    //   allowNull: false,
+    // },
+    //
+    // language_id: {
+    //   Sequelize.INTEGER(11),
+    //   allowNull: false,
+    // },
+
+    tag: {
+      Sequelize.STRING(32),
+      allowNull: false,
+    },
+
+  },
+  associations: function() {
+
+  },
+  options: {
+    paranoid: true,
+    classMethods: {
+      deleteById: async (id) => {
+        try {
+          return await ProductTag.destroy({ where: { id } });
+        } catch (e) {
+          sails.log.error(e);
+          throw e;
+        }
+      },
+    },
+    instanceMethods: {},
+    hooks: {}
+  }
+}
