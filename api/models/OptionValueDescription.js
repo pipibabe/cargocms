6d867795afabfa94cc1f67e324c437b62078eea0
KenYA <tikka0910@gmail.com>
2016-12-07 17:57:22 +0800

    Option Value Description 增加與 Option , OptionValue 關聯

--- api/models/OptionValueDescription.js
+++ api/models/OptionValueDescription.js
@@ -21,7 +21,8 @@ module.exports = {
     },
   },
   associations: function() {
-
+    OptionValueDescription.belongsTo(Option);
+    OptionValueDescription.belongsTo(OptionValue);
   },
   options: {
     classMethods: {
