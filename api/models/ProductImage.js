88d7a71c53311a846e20b13a5782c5e22c66cdd4
KenYA <tikka0910@gmail.com>
2016-12-08 14:00:32 +0800

    建立ProductImage 資料表

---
+++ api/models/ProductImage.js
@@ -0,0 +1,39 @@
+module.exports = {
+  attributes: {
+
+    // productId: {
+    //   type: Sequelize.INTEGER(11),
+    //   allowNull: false,
+    // },
+
+    image: {
+      type: Sequelize.STRING(255),
+      defaultValue: null,
+    },
+
+    sortOrder: {
+      type: Sequelize.INTEGER(3),
+      allowNull: false,
+      defaultValue: 0,
+    },
+
+  },
+  associations: function() {
+
+  },
+  options: {
+    classMethods: {
+      deleteById: async (id) => {
+        try {
+          return await ProductImage.destroy({ where: { id } });
+        } catch (e) {
+          sails.log.error(e);
+          throw e;
+        }
+      },
+    },
+    instanceMethods: {},
+    hooks: {}
+  }
+
+}
