0534c582fe64f6180d88fb74b0ed2673848ef921
FuYaoDe <dan82625@gmail.com>
2016-12-12 15:03:50 +0800

    zone to geo zone

--- api/models/Zone.js
+++ api/models/Zone.js
@@ -16,6 +16,7 @@ module.exports = {
     },
   },
   associations: () => {
+    Zone.hasOne(ZoneToGeoZon),
   },
   options: {
     classMethods: {},
--- api/models/ZoneToGeoZon.js
+++ api/models/ZoneToGeoZon.js
@@ -1,21 +1,8 @@
 
 module.exports = {
-  attributes: {
-    name: {
-      type: Sequelize.STRING(128),
-      allowNull: false,
-    },
-    code: {
-      type: Sequelize.STRING(32),
-      allowNull: false,
-    },
-    status: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
-    },
-  },
+  attributes: {},
   associations: () => {
+    ZoneToGeoZon.belongsTo(Country),
   },
   options: {
     classMethods: {},
