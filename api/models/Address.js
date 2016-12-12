5d1a11c6ca8d4fa5153ed09177561a04a52f7760
FuYaoDe <dan82625@gmail.com>
2016-12-12 14:02:00 +0800

    add Address

---
+++ api/models/Address.js
@@ -0,0 +1,48 @@
+
+module.exports = {
+  attributes: {
+    customerId: {
+
+    },
+    firstname: {
+      type: Sequelize.STRING(32),
+      allowNull: false,
+    },
+    lastname: {
+      type: Sequelize.STRING(32),
+      allowNull: false,
+    },
+    company: {
+      type: Sequelize.STRING(40),
+      allowNull: false,
+    },
+    address1: {
+      type: Sequelize.STRING(128),
+      allowNull: false,
+    },
+    address2: {
+      type: Sequelize.STRING(128),
+      allowNull: false,
+    },
+    city: {
+      type: Sequelize.STRING(10),
+      allowNull: false,
+    },
+    postcode: {
+      type: Sequelize.STRING(10),
+      allowNull: false,
+    },
+    customField: {
+      type: Sequelize.INTEGER(11),
+      allowNull: false,
+    },
+  },
+  associations: () => {
+    Address.belongsTo(User),
+  },
+  options: {
+    classMethods: {},
+    instanceMethods: {},
+    hooks: {}
+  }
+};
