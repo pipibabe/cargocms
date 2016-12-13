5a4b51fe08248ed902fa709437de2aa8cad6a68c
KenYA <tikka0910@gmail.com>
2016-12-13 13:42:54 +0800

    將 model 補上格式化時間格式

--- api/models/OptionValue.js
+++ api/models/OptionValue.js
@@ -15,6 +15,28 @@ module.exports = {
       type: Sequelize.INTEGER(3),
       allowNull: false
     },
+
+    createdDateTime:{
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('createdAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    },
+
+    updatedDateTime:{
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('updatedAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    }
   },
   associations: function() {
     // OptionValue.belongsTo(Option);
--- api/models/ProductDescription.js
+++ api/models/ProductDescription.js
@@ -39,6 +39,28 @@ module.exports = {
       type: Sequelize.STRING(255),
       allowNull: false,
     },
+
+    createdDateTime:{
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('createdAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    },
+
+    updatedDateTime:{
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('updatedAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    }
   },
   associations: function() {
     // ProductDescription.belongsTo(Product);
