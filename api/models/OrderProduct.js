ddd9dbbd98918f022408c5e7f937cdcf601c2d19
KenYA <tikka0910@gmail.com>
2016-12-14 16:22:48 +0800

    Order Product 增加 格式化時間

--- api/models/OrderProduct.js
+++ api/models/OrderProduct.js
@@ -32,6 +32,28 @@ module.exports = {
       type: Sequelize.INTEGER(8),
       allowNull: false,
     },
+
+    createdDateTime:{
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('createdAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    },
+
+    updatedDateTime:{
+      type: Sequelize.VIRTUAL,
+      get: function(){
+        try{
+          return UtilsService.DataTimeFormat(this.getDataValue('updatedAt'));
+        } catch(e){
+          sails.log.error(e);
+        }
+      }
+    }
   },
   associations: () => {
     OrderProduct.belongsTo(Product);
