66859cf1e2dc9296de50608b78ea5d50d1d76d99
FuYaoDe <dan82625@gmail.com>
2016-12-19 17:10:34 +0800

    新增 shopCard

--- react-app-ship/src/views/ShipListView/ShipCard.js
+++ react-app-ship/src/views/ShipListView/ShipCard.js
@@ -1,64 +1,100 @@
 import React, { PropTypes } from 'react';
 import {
   Card,
-  CardActions,
-  CardHeader,
   CardText,
 } from 'material-ui/Card';
-import { FlatButton } from 'material-ui';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+// import classes from './Card.scss'
+
+const classes = {
+  title: {
+    width: '25%',
+    height: '70px',
+    float: 'left',
+    padding: 10,
+  },
+  desc: {
+    width: '50%',
+    height: '70px',
+    float: 'left',
+    padding: 10,
+    textAlign: 'left',
+  },
+  descTitle: {
+    fontSize: '15px',
+  },
+  price: {
+    width: '10%',
+    height: '70px',
+    float: 'left',
+    padding: 10,
+    textAlign: 'center',
+    lineHeight: '50px',
+  },
+  status: {
+    width: '15%',
+    height: '70px',
+    float: 'left',
+    padding: 10,
+    lineHeight: '50px',
+  }
+}
 
 export default class ShipCard extends React.Component {
-  static defaultProps = {
+   static defaultProps = {
     id: 0,
     orderNum: 'S1111222233334444',
     orderDate: '2015/12/12',
     title: '一六八活海產(04)-0000-0000',
     desc: '鮮甜飽滿無毒益菌蝦(3) + 巨無霸完美天使紅蟹(2)',
-    price: '$2,050',
+    price: '2,050',
     status: '新訂單',
   };
 
-  static propTypes = {
-    id: PropTypes.number.isRequired,
-    orderNum: PropTypes.String.isRequired,
-    orderDate: PropTypes.String.isRequired,
-    title: PropTypes.String.isRequired,
-    desc: PropTypes.String.isRequired,
-    price: PropTypes.String.isRequired,
-    status: PropTypes.String.isRequired,
+   static propTypes = {
+    id: PropTypes.number,
+    orderNum: PropTypes.string,
+    orderDate: PropTypes.string,
+    title: PropTypes.string,
+    desc: PropTypes.string,
+    price: PropTypes.string,
+    status: PropTypes.string,
   };
 
   constructor(props, context) {
     super(props, context);
-    injectTapEventPlugin();
     this.state = {
       open: false,
     };
   }
 
   render() {
+    const { orderNum, orderDate, title, desc, price, status } = this.props;
+    const { open } = this.state;
     return (
-      <div className='d' >
-        <Card>
-          <CardHeader
-            title='Without Avatar'
-            subtitle='Subtitle'
-            actAsExpander={true}
-            showExpandableButton={true}
-          />
-          <CardActions>
-            <FlatButton label='Action2' />
-          </CardActions>
-          <CardText expandable={true}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-        </Card>
-        <FlatButton label='Action1' />
-      </div>
+      <Card expanded={this.state.open} onClick={() => { this.setState({ open: !open }) }}>
+        <div>
+          <div style={classes.title}>
+            <div>{orderNum}</div>
+            <div>建立於 {orderDate}</div>
+          </div>
+          <div style={classes.desc}>
+            <div style={classes.descTitle}>{title}</div>
+            <div>{desc}</div>
+          </div>
+          <div style={classes.price}>
+            <div>${price}</div>
+          </div>
+          <div style={classes.status}>
+            <div>{status}</div>
+          </div>
+        </div>
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+      </Card>
     );
   }
 
