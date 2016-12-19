2da3ae5863f3734330e22631cdab9309f334b690
Kuyen <iamcxa@gmail.com>
2016-12-19 14:45:32 +0800

    update router

--- react-app-ship/src/routes/index.js
+++ react-app-ship/src/routes/index.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
 
 // NOTE: here we're making use of the `resolve.root` configuration
 // option in webpack, which allows us to specify import paths as if
 // they were from the root of the ~/src directory. This makes it
 // very easy to navigate to files regardless of how deeply nested
 // your current file is.
-import CoreLayout from 'layouts/CoreLayout/CoreLayout'
-import HomeView from 'views/HomeView/HomeView'
-import Login from 'views/Login/Login'
-import MaterialUi from 'views/MaterialUi/MaterialUi'
+import CoreLayout from 'layouts/CoreLayout/CoreLayout';
+import HomeView from 'views/HomeView/HomeView';
+import Login from 'views/Login/Login';
+import MaterialUi from 'views/MaterialUi/MaterialUi';
+import ShipList from 'views/ShipListView/ShipList';
 
 export default (store) => (
   <Route path='/'>
-    <Route path="/ship/login" component={Login} />
+    <Route path='/ship/login' component={Login} />
     <Route path='/ship' component={CoreLayout}>
-      <IndexRoute component={HomeView} />
-      <Route path="index" component={HomeView} />
-      <Route path='MaterialUi' component={MaterialUi} />    <IndexRoute component={HomeView} />
+      <IndexRoute component={ShipList} />
+      {/* <Route path='index' component={ShipList} /> */}
+      <Route path='MaterialUi' component={MaterialUi} />
+      <Route path='HomeView' component={HomeView} />
+      <Route path='ShipList' component={ShipList} />
     </Route>
   </Route>
-)
+);
 
 //
 // export default (store) => (
