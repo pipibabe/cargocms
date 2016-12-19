e27e515d3f8a255b24ed8bc462cfe31d53c90965
KenYA <tikka0910@gmail.com>
2016-12-19 15:38:45 +0800

    增加測試供應商資訊

--- config/init/b2b/index.js
+++ config/init/b2b/index.js
@@ -1,8 +1,10 @@
 import product from './product/index.js';
+import supplier from './supplier/index.js';
 import order from './order/index.js';
 import orderProduct from './orderproduct/index.js';
 
 module.exports.init = async function(){
+  await supplier.init();
   await product.init();
   await order.init();
   await orderProduct.init();
