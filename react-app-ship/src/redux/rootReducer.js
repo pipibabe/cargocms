2efac4f2b4a5267a994cefbc4e65b3fa69839306
Kuyen <iamcxa@gmail.com>
2016-12-20 18:16:57 +0800

    add shipOrder and insert it into rootReducer just in case

--- react-app-ship/src/redux/rootReducer.js
+++ react-app-ship/src/redux/rootReducer.js
@@ -1,8 +1,10 @@
-import { combineReducers } from 'redux'
-import { routerReducer as router } from 'react-router-redux'
-import counter from './modules/counter'
+import { combineReducers } from 'redux';
+import { routerReducer as router } from 'react-router-redux';
+import counter from './modules/counter';
+import shipOrder from './modules/shipOrder';
 
 export default combineReducers({
+  shipOrder,
   counter,
-  router
-})
+  router,
+});
