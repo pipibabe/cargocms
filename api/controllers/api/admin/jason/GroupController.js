e5c69c4cb23b3399b42172ca3c1388fa3de19348
KenYA <tikka0910@gmail.com>
2016-12-22 12:46:45 +0800

    Group Controller 增加判斷 「實例照片」無法刪除。

--- api/controllers/api/admin/jason/GroupController.js
+++ api/controllers/api/admin/jason/GroupController.js
@@ -82,6 +82,10 @@ module.exports = {
   destroy: async (req, res) => {
     try {
       const { id } = req.params;
+      const group = Group.findById(id);
+      if( group.title === '實例照片'){
+        throw Error('實例照片無法刪除');
+      }
       const item = await Group.deleteById(id);
       const message = 'Delete success.';
       res.ok({ message, data: { item } });
