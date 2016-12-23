8f7fce136922ce1ae8a0ce043a9a6c3fe2e9b5f9
sakuxz <yui12327@gmail.com>
2016-12-23 13:30:23 +0800

    fix eslint

--- react-app-ship/src/views/Login/Login.js
+++ react-app-ship/src/views/Login/Login.js
@@ -1,10 +1,10 @@
 /* @flow */
-import React, { PropTypes } from 'react'
-import { connect } from 'react-redux'
-import { increment, doubleAsync } from '../../redux/modules/counter'
-import Crab from './crab.png'
-import FishLogo from './fish logo.png'
-import classes from './_style.scss'
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { increment, doubleAsync } from '../../redux/modules/counter';
+import Crab from './crab.png';
+import FishLogo from './fish logo.png';
+import classes from './_style.scss';
 
 // We can use Flow (http://flowtype.org/) to type our component's props
 // and state. For convenience we've included both regular propTypes and
@@ -22,54 +22,54 @@ type Props = {
 // We avoid using the `@connect` decorator on the class definition so
 // that we can export the undecorated component for testing.
 // See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
-export class HomeView extends React.Component {
-  props: Props;
+export class Login extends React.Component {
   static propTypes = {
     counter: PropTypes.number.isRequired,
     doubleAsync: PropTypes.func.isRequired,
     increment: PropTypes.func.isRequired
   };
+  props: Props;
 
-  render () {
+  render() {
     return (
       <div className='login-container' >
-      <div className="login-form">
-        <div className="login-form-body">
-          <h1>出貨管理</h1>
-          <img className="crab" src={Crab} alt="" />
-          <form>
-            <label>帳號</label>
-            <input type="text" placeholder="Username" className="form-control margin-bottom-20" />
-            <label>密碼</label>
-            <input type="text" placeholder="Password" className="form-control" />
-            <a className="forget-password" href="#">忘記密碼？</a>
-            <button type="button" className="btn login-btn">登入系統</button>
-          </form>
-        </div>
-        <div className="login-form-footer">
-          <div className="login-contact">
-            <img src={FishLogo} />
-            <div className="login-contact-info">
-              <h1>雲端漁場服務專線</h1>
-              <p>
-                <a href="#">(05)0000-0000</a>
-              </p>
-            </div>
+        <div className='login-form'>
+          <div className='login-form-body'>
+            <h1>出貨管理</h1>
+            <img className='crab' src={Crab} alt='crab' />
+            <form>
+              <label>帳號</label>
+              <input type='text' placeholder='Username' className='form-control margin-bottom-20' />
+              <label>密碼</label>
+              <input type='text' placeholder='Password' className='form-control' />
+              <a className='forget-password' href='#'>忘記密碼？</a>
+              <button type='button' className='btn login-btn'>登入系統</button>
+            </form>
           </div>
-          <a href="#" className="facebook-btn" >
-            <i className="fa fa-facebook" aria-hidden="true"></i>
-          </a>
+          <div className='login-form-footer'>
+            <div className='login-contact'>
+              <img src={FishLogo} alt='fish logo' />
+              <div className='login-contact-info'>
+                <h1>雲端漁場服務專線</h1>
+                <p>
+                  <a href='#'>(05)0000-0000</a>
+                </p>
+              </div>
+            </div>
+            <a href='#' className='facebook-btn' >
+              <i className='fa fa-facebook' aria-hidden='true' />
+            </a>
+          </div>
         </div>
       </div>
-      </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => ({
-  counter: state.counter
-})
+const mapStateToProps = state => ({
+  counter: state.counter,
+});
 export default connect(mapStateToProps, {
   increment: () => increment(1),
-  doubleAsync
-})(HomeView)
+  doubleAsync,
+})(Login);
