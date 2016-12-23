8e9203e8988ce89f0e167e8dc4dc1c01f646c761
Kuyen <iamcxa@gmail.com>
2016-12-23 09:32:41 +0800

    add toast

--- react-app-ship/src/redux/rootReducer.js
+++ react-app-ship/src/redux/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
 import counter from './modules/counter';
 import shipOrder from './modules/shipOrder';
+import toastReducer from './modules/toast';
 
 export default combineReducers({
   shipOrder,
   counter,
+  toastReducer,
   router,
 });
