2da204c76325c2cb0085b9c4ff803bf81f87deb2
Kuyen <iamcxa@gmail.com>
2016-12-26 15:31:09 +0800

    add search action

--- react-app-ship/src/redux/utils/fetchApi.js
+++ react-app-ship/src/redux/utils/fetchApi.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 
 export async function postData(api: string, data: Object = {}) {
   return await axios.post(api, data)
-      .then(response => response)
-      .catch(error => error);
+    .then(response => response)
+    .catch(error => error);
 }
 
 export async function getData(api: string, params: Object = {}) {
-  return await axios.post(api, { params })
+  return await axios.get(api, { params })
     .then(response => response)
     .catch(error => error);
 }
