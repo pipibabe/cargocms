a2d114cac11dbd42ddbfa4fee980dfe7e133624e
KenYA <tikka0910@gmail.com>
2016-12-26 14:58:17 +0800

    調整 paymentAddressFormat , pyamentCustomField 欄位型態 。註解 Order Status Id 欄位

--- api/models/OrderPayment.js
+++ api/models/OrderPayment.js
@@ -1,125 +1,125 @@
 module.exports = {
   attributes: {
-    
+
       paymentFirstname: {
         type: Sequelize.STRING(32),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentLastname: {
         type: Sequelize.STRING(32),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentCompany: {
         type: Sequelize.STRING(32),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentAddress1: {
         type: Sequelize.STRING(128),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentAddress2: {
         type: Sequelize.STRING(128),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentCity: {
         type: Sequelize.STRING(128),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentPostcode: {
         type: Sequelize.STRING(10),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentCountry: {
         type: Sequelize.STRING(128),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentCountryId: {
         type: Sequelize.INTEGER(11),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentZone: {
         type: Sequelize.STRING(128),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentZoneId: {
         type: Sequelize.INTEGER(11),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentAddressFormat: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentCustomField: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentMethod: {
         type: Sequelize.STRING(128),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       paymentCode: {
         type: Sequelize.STRING(128),
         allowNull: false,
-        
-        
+
+
       },
-  
+
       status: {
         type: Sequelize.STRING(32),
         allowNull: false,
-        
-        
+
+
       },
-  
-      OrderStatusId: {
-        type: Sequelize.INTEGER(11),
-        allowNull: false,
-        
-        
-      },
-  
+
+      // OrderStatusId: {
+      //   type: Sequelize.INTEGER(11),
+      //   allowNull: false,
+      //
+      //
+      // },
+
 
     createdDateTime:{
       type: Sequelize.VIRTUAL,
