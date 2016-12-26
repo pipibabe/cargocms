1ba8255f5b4fa4b766cf43e6b850c9a7161cf4f5
Kuyen <iamcxa@gmail.com>
2016-12-26 15:31:19 +0800

    add search action

--- react-app-ship/src/redux/modules/shipOrder.js
+++ react-app-ship/src/redux/modules/shipOrder.js
@@ -1,18 +1,19 @@
-/* @flow */
-import { postData } from '../utils/fetchApi';
+import {
+  postData,
+  getData,
+} from '../utils/fetchApi';
 import { showToast } from './toast';
 // ------------------------------------
 // Constants
 // ------------------------------------
 export const GET_SHIP_LIST = 'GET_SHIP_LIST';
-export const FETCH_SHIP_LIST_API = 'FETCH_SHIP_LIST_API';
+export const FIND_SHIP_ITEM = 'FIND_SHIP_ITEM';
 
 // ------------------------------------
 // Actions
 // ------------------------------------
-
 export function deliverShipListData(data) {
-  console.log('data=>', data);
+  console.log('deliverShipListData=>', data);
   return {
     type: GET_SHIP_LIST,
     list: data,
@@ -20,7 +21,7 @@ export function deliverShipListData(data) {
 }
 
 export function fetchShipListData() {
-  return async(dispatch, getState)=> {
+  return async(dispatch) => {
     const api = '/api/admin/suppliershiporder/all';
     const fetchResult = await postData(api);
     let result = '';
@@ -37,8 +38,54 @@ export function fetchShipListData() {
       }
       dispatch(showToast(result));
     }
-    // console.log('fetchResult=>', fetchResult);
-    // console.log('result=>', result);
+  };
+}
+
+export function deliverFindShipItem(searchText, data) {
+  console.log('deliverFindShipItem=>', data);
+  return {
+    type: FIND_SHIP_ITEM,
+    list: data,
+    searchText,
+  };
+}
+
+export function fetchFindShipItem(value) {
+  console.log('search value=>', value);
+  return async(dispatch, getState) => {
+    const api = '/api/admin/suppliershiporder/';
+    const query = {
+      startDate: '1900/01/01',
+      endDate: '3000/01/01',
+      columns:[
+        { data: 'displayName', searchable: true },
+        { data: 'email', searchable: true },
+        { data: 'telephone', searchable: true },
+        { data: 'paymentAddress1', searchable: true },
+        { data: 'paymentCity', searchable: true }
+      ],
+      order: [ { column: '0', dir: 'asc' } ],
+      search: { value, regex: 'false' },
+      _: '1470989140227'
+    };
+    const fetchResult = await getData(api, query);
+    let result = '';
+    // success
+    if (fetchResult.status) {
+      dispatch(deliverFindShipItem(value, fetchResult.data.data));
+      dispatch(showToast('載入完成'));
+    } else {
+      // error
+      if (fetchResult.response) {
+        result = fetchResult.response.statusText;
+      } else {
+        result = fetchResult.message;
+      }
+      dispatch(showToast(result));
+    }
+    console.log('data query=>', JSON.stringify(query));
+    console.log('fetchResult=>', fetchResult);
+    console.log('result=>', result);
   };
 }
 
@@ -54,6 +101,11 @@ export const ACTION_HANDLERS = {
   [GET_SHIP_LIST]: (state = {}, action) => ({
     ...state,
     list: action.list,
+  }),
+  [FIND_SHIP_ITEM]: (state = {}, action) => ({
+    ...state,
+    list: action.list,
+    searchText: action.searchText,
   }),
 };
 
