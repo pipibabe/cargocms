218fcfa3920a4e686945da04bdc5da830f33bc30
KenYA <tikka0910@gmail.com>
2016-12-26 16:22:01 +0800

    新增 OrderPayment 測試資料

--- config/init/b2b/index.js
+++ config/init/b2b/index.js
@@ -3,6 +3,7 @@ import supplier from './supplier/index.js';
 import supplierShipOrder from './suppliershiporder/index.js';
 import order from './order/index.js';
 import orderProduct from './orderproduct/index.js';
+import orderPayment from './orderpayment/index.js';
 
 module.exports.init = async function(){
   await supplier.init();
@@ -10,4 +11,5 @@ module.exports.init = async function(){
   await order.init();
   await orderProduct.init();
   await supplierShipOrder.init();
+  await orderPayment.init();
 }
