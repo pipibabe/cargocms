98d7bb0e575b2c67bcd561351422d92064a8c7c5
KenYA <tikka0910@gmail.com>
2016-12-27 15:38:35 +0800

    修改，增加 mockadmin 登入 增加 Product 5

--- test/unit/controllers/api/ProductController.spec.js
+++ test/unit/controllers/api/ProductController.spec.js
@@ -1,13 +1,15 @@
 var sinon = require('sinon');
 import {mockAdmin, unMockAdmin} from "../../../util/adminAuthHelper.js"
+import createHelper from "../../../util/createHelper.js"
 
-describe.skip('about Product Controller operations.', function() {
+describe('about Product Controller operations.', function() {
   let image, product1, product2, product3, product4, product5, user;
   let supplier1, supplier2;
   let category1, category2, Category3;
 
   before(async (done) => {
     try {
+      await mockAdmin();
 
       image = await Image.create({
         filePath: 'http://www.labfnp.com/modules/core/img/update1.jpg',
@@ -60,6 +62,18 @@ describe.skip('about Product Controller operations.', function() {
         ImageId: image.id
       };
 
+      product5 = {
+        model: 'product5',
+        sku: 1,
+        upc: 1,
+        ean: 1,
+        jan: 1,
+        isbn: 1,
+        mpn: 1,
+        location: 1,
+        ImageId: image.id
+      };
+
       category1 = await createHelper.supplierCategory('海產');
       category2 = await createHelper.supplierCategory('山產');
 
@@ -102,7 +116,7 @@ describe.skip('about Product Controller operations.', function() {
       const res = await request(sails.hooks.http.app)
       .get(`/api/product?start=0&length=10&category=${category1.id}&supplier=${supplier1.id}`);
       res.status.should.be.eq(200);
-      res.body.items.length.should.be.eq(2);
+      res.body.data.items.length.should.be.eq(2);
       done();
     } catch (e) {
       done(e);
