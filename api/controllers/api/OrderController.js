ae1c06d9cd6d645bb9f2fb177224129e11066b5a
KenYA <tikka0910@gmail.com>
2016-12-29 16:13:26 +0800

    將 Order Controller 內建立 Order的處理移動至 OrderService.createOrder

--- api/controllers/api/OrderController.js
+++ api/controllers/api/OrderController.js
@@ -3,39 +3,11 @@ module.exports = {
     try{
       let data = req.body;
       const loginUser = AuthService.getSessionUser(req);
-      const products = data.products;
 
-      // data remove products
-      delete data.products;
-
-      const orderPaymentStatus = await OrderPaymentStatus.findOne({where: {name: 'NEW'}});
-
-      const orderPayment = await OrderPayment.create({
-        paymentFirstname: data.paymentFirstname,
-        paymentLastname: data.paymentLastname,
-        paymentCompany: data.paymentCompany,
-        paymentAddress1: data.paymentAddress1,
-        paymentAddress2: data.paymentAddress2,
-        paymentCity: data.paymentCity,
-        paymentPostcode: data.paymentPostcode,
-        paymentCountry: data.paymentCountry,
-        paymentCountryId: data.paymentCountryId,
-        paymentZone: data.paymentZone,
-        paymentZoneId: data.paymentZoneId,
-        paymentAddressFormat: data.paymentAddressFormat,
-        paymentCustomField: data.paymentCustomField,
-        paymentMethod: data.paymentMethod,
-        paymentCode: data.paymentCode,
-        OrderPaymentStatusId: orderPaymentStatus.id
-      });
-
-      await OrderPaymentHistory.create({
-        status: orderPaymentStatus.name,
-        comment: 'create new Order',
-        notify: true,
-        OrderPaymentId: orderPayment.id,
-        OrderPaymentStatusId: orderPaymentStatus.id
-      });
+      if( data.firstname !== loginUser.firstName || data.lastname  !== loginUser.lastName){
+        data.firstname = loginUser.firstName;
+        data.lastname  = loginUser.lastName;
+      }
 
       // some data can fetch from request
       data.userAgent = req.header["user-agent"] || '';
@@ -45,46 +17,12 @@ module.exports = {
       // data.forwardedIp = req.headers["X-Forwarded-For"] || '';
       data.acceptLanguage = req.header["accept-language"] || '';
 
-      console.log(data);
+      const { order , orderProduct } = await OrderService.createOrder(data);
 
-      //const user = await User.findById(loginUser.id);
-      data.firstname = loginUser.firstName;
-      data.lastname  = loginUser.lastName;
-
-      console.log("=== make order");
-      const order = await Order.create({
-        ...data,
-        OrderPaymentId: orderPayment.id
-      });
-
-      sails.log.info("new Order Create", order);
-
-      for(let p of products){
-        let product = await Product.find({
-          where: {
-            id: p.id,
-          },
-          include: ProductDescription
-        });
-        await OrderProduct.create({
-          ProductId: product.id,
-          OrderId: order.id,
-          name: product.ProductDescription.name,
-          model: product.model,
-          quantity: p.quantity,
-          price: product.price,
-          total: (product.price * p.quantity),
-          tax: (product.price * p.quantity) * 0.05,
-          // reward: 0
-        });
-      }
       const message = 'Order create success';
-      res.ok({
-        message: message,
-        data: {
-          item: order
-        }
-      });
+
+      return res.redirect(`/orderinfo/${order.id}`);
+
     } catch (e) {
       res.serverError(e);
     }
@@ -101,13 +39,15 @@ module.exports = {
         }
       })
       const message = 'get Order info success';
-      res.ok({
-        message,
+
+      res.view('b2b/order/index',{
+        message: message,
         data: {
           item: order,
-          product: orderProduct
+          product: orderProduct,
         }
-      })
+      });
+
     } catch(e) {
       res.serverError(e);
     }
