61319d90be085541d2f47344a08ae589e2a7517d
KenYA <tikka0910@gmail.com>
2016-12-30 11:34:11 +0800

    修改 code style ， 讓 function 傳進去的參數更加明確

--- config/init/b2b/product/index.js
+++ config/init/b2b/product/index.js
@@ -7,13 +7,13 @@ module.exports.init = async () => {
 
     if (isDevMode && isDropMode) {
 
-      await productCreateHelper.create('鮮甜飽滿無毒益菌蝦','壹陸捌活海產', '蝦子', 'shrimp');
+      await productCreateHelper.create({ name:'鮮甜飽滿無毒益菌蝦', supplierName:'壹陸捌活海產', categoryType:'蝦子', categoryEng:'shrimp'});
 
-      await productCreateHelper.create('鮮甜飽滿大扇貝','一尾活海鮮', '貝殼干貝' , 'mussel');
+      await productCreateHelper.create({ name:'鮮甜飽滿大扇貝', supplierName:'一尾活海鮮', categoryType:'貝殼干貝' , categoryEng:'mussel'});
 
-      await productCreateHelper.create('綿密司目魚','鱻海鮮', '司目魚','milkfish');
+      await productCreateHelper.create({ name:'綿密司目魚', supplierName:'鱻海鮮', categoryType:'司目魚', categoryEng:'milkfish'});
 
-      await productCreateHelper.create('清甜透抽小卷','狠鮮海產', '透抽小卷','cuttlefish');
+      await productCreateHelper.create({ name:'清甜透抽小卷', supplierName:'狠鮮海產', categoryType:'透抽小卷', categoryEng:'cuttlefish'});
 
     }
 
